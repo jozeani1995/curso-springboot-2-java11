75e7b11d3ed3b3f84bc24216a94f4c48b1c7c998
jozeani <jozeani1995maria@gmail.com>
2021-01-27 15:46:52 -0300

    banco de dados h2, teste profile, jpa

--- src/main/java/com/educandoweb/curso/entities/Usuario.java
+++ src/main/java/com/educandoweb/curso/entities/Usuario.java
@@ -2,9 +2,20 @@ package com.educandoweb.curso.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+//anotacao do JPA que essa classe ENTIDADE reprenta uma uma tabela no banco de dados
 public class Usuario implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+//	mostrando que o id é chave primaria
+	@Id
+//	usar o generateValue para mostrar que a chave é auto incrementavel
+	@GeneratedValue(strategy = GenerationType.IDENTITY )
 	private Long id;
 	private String name;
 	private String email;
--- src/main/java/com/educandoweb/curso/resources/UsuarioResource.java
+++ src/main/java/com/educandoweb/curso/resources/UsuarioResource.java
@@ -9,11 +9,15 @@ import com.educandoweb.curso.entities.Usuario;
 
 @RestController
 @RequestMapping(value = "/usuarios")
+// caminho do recurso
 public class UsuarioResource {
 
 	@GetMapping
+//	responseEntity = respostas de requisições web
 	public ResponseEntity<Usuario> buscarTodos(){
 		Usuario u = new Usuario(1L, "bob", "bob@gmail.com", "123", "555555");
+//		ok = resposta positiva 
+//		body = mostrar o corpo instanciado do usuario com os dados
 		return ResponseEntity.ok().body(u);
 	}
 }
